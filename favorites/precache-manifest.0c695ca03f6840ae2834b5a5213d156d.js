6cbd7e5496187aef79adfe43ddb60cc68887421a
Andrei Lebedev <a.lebedev@dataduck.com>
2019-02-04 11:22:16 +0300

    fixed routing on pages

---
+++ favorites/precache-manifest.0c695ca03f6840ae2834b5a5213d156d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ba65588c38c3d499f0a",
+    "url": "/kudago-test/static/js/runtime~main.5ba65588.js"
+  },
+  {
+    "revision": "d0eaa861572c371bbdf2",
+    "url": "/kudago-test/static/js/main.d0eaa861.chunk.js"
+  },
+  {
+    "revision": "d2559d5555f083de0808",
+    "url": "/kudago-test/static/js/1.d2559d55.chunk.js"
+  },
+  {
+    "revision": "d0eaa861572c371bbdf2",
+    "url": "/kudago-test/static/css/main.7ad9db7f.chunk.css"
+  },
+  {
+    "revision": "2723d14dda5f9aa44c6921e13dedc349",
+    "url": "/kudago-test/index.html"
+  }
+];
